1ce5d3d84f18105394a583d37a2ec3bf2ede1047
terzeron <terzeron@gmail.com>
2020-10-07 15:44:41 +0900

    add an example for enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,4 +1,4 @@
-fn enum_test() {
+fn enum_test1() {
     #![allow(dead_code)]
     enum Person {
         Engineer,
@@ -7,8 +7,8 @@ fn enum_test() {
         Weight(i32),
         Info {
             name: String,
-            height: i32
-        }
+            height: i32,
+        },
     }
 
     fn inspect(p: Person) {
@@ -17,7 +17,7 @@ fn enum_test() {
             Person::Scientist => println!("is a scientist!"),
             Person::Height(i) => println!("has a height of {}", i),
             Person::Weight(i) => println!("has a weight of {}", i),
-            Person::Info { name, height} => { println!("{} is {} tall.", name, height); },
+            Person::Info { name, height } => { println!("{} is {} tall.", name, height); }
         }
     }
 
@@ -33,7 +33,35 @@ fn enum_test() {
     inspect(rohan);
 }
 
+fn enum_test2() {
+    #![allow(dead_code)]
+    enum Status {
+        Rich,
+        Poor,
+    }
+    enum Work {
+        Civilian,
+        Soldier,
+    }
+
+    use Status::{Poor, Rich};
+    use Work::*;
+
+    let status = Poor;
+    let work = Civilian;
+    match status {
+        Rich => println!("The rich have lots of money!"),
+        Poor => println!("The poor have no money!"),
+    }
+    match work {
+        Civilian => println!("Civilians work!"),
+        Soldier => println!("Soldiers fight!"),
+    }
+}
+
 fn main() {
-    println!("--------- enum test ---------");
-    enum_test()
+    println!("--------- enum_test1 ---------");
+    enum_test1();
+    println!("--------- enum_test2 ---------");
+    enum_test2();
 }
