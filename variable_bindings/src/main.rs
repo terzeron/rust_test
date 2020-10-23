33cccbfef54aaa964bf3745a554b8dc46ec3a4c8
terzeron <terzeron@gmail.com>
2020-10-23 09:16:15 +0900

    add an exmaple for variable bindings

--- variable_bindings/src/main.rs
+++ variable_bindings/src/main.rs
@@ -1,3 +1,14 @@
+fn mutability_test() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+    mutable_binding += 1;
+    println!("After mutation: {}", mutable_binding);
+
+    //_immutable_binding + 1; // error
+}
 fn main() {
-    println!("Hello, world!");
+    println!("------ mutability_test() ------");
+    mutability_test();
 }
