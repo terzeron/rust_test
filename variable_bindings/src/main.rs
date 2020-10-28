b41ec49dd990000f8775895ed65ce5997c805f67
terzeron <terzeron@gmail.com>
2020-10-28 17:14:06 +0900

    merge a module to another

--- variable_bindings/src/main.rs
+++ variable_bindings/src/main.rs
@@ -8,7 +8,27 @@ fn mutability_test() {
 
     //_immutable_binding + 1; // error
 }
+
+fn scope_shadowing_test() {
+    let long_lived_binding = 1;
+
+    {
+        let short_lived_binding = 2;
+        println!("inner short: {}", short_lived_binding);
+        let long_lived_binding = 5_f32;
+        println!("inner long: {}", long_lived_binding);
+    }
+
+    //println!("outer short: {}", short_lived_binding); // error
+    println!("outer long: {}", long_lived_binding);
+
+    let long_lived_binding = 'a';
+    println!("outer long: {}", long_lived_binding);
+}
+
 fn main() {
     println!("------ mutability_test() ------");
     mutability_test();
+    println!("------ scope_shadowing_test() ------");
+    scope_shadowing_test();
 }
