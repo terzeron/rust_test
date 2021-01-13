8c27702545b50d478fe2f5c7d528e558d8b69b0c
terzeron <terzeron@gmail.com>
2021-01-13 16:06:39 +0900

    add an exmaple for if-else and loop

--- flow_control/src/main.rs
+++ flow_control/src/main.rs
@@ -1,4 +1,27 @@
 fn if_else_test() {
+    let n = 5;
+
+    if n < 0 {
+        println!("{} is negative", n);
+    } else if n > 0 {
+        println!("{} is positive", n);
+    } else {
+        println!("{} is zero", n);
+    }
+
+    let big_n =
+        if n < 10 && n > -10 {
+            println!(", and is a small number, increase ten-fold");
+            10 * n
+        } else {
+            println!(", and is a big number, halve the number");
+            n / 2
+        };
+
+    println!("{} -> {}", n, big_n);
+}
+
+fn loop_test() {
     let mut count = 0u32;
     println!("Let's count until infinity!");
 
@@ -17,5 +40,8 @@ fn if_else_test() {
 }
 
 fn main() {
+    println!("\n------ if_else_test() ------");
     if_else_test();
+    println!("\n------ loop_test() ------");
+    loop_test();
 }
