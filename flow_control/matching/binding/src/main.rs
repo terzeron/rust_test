042c9d41c5612065a92614f4c60bef27e62b0fb8
terzeron <terzeron@gmail.com>
2021-04-06 13:59:30 +0900

    add some examples for guard and binding of match

--- flow_control/matching/binding/src/main.rs
+++ flow_control/matching/binding/src/main.rs
@@ -1,3 +1,19 @@
+fn binding_test() {
+    fn age() -> u32 {
+        15
+    }
+
+    println!("Tell me type of person you are");
+    match age() {
+        0 => println!("I'm not born yet I guess"),
+        // @를 이용하여 값을 변수에 바인딩함
+        n @ 1...12 => println!("I'm a child of age {:?}", n),
+        n @ 13...19 => println!("I'm a teen of age {:?}", n),
+        n => println!("I'm an old person of age {:?}", n),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("\n------ binding_test() ------");
+    binding_test();
 }
--- flow_control/matching/guards/src/main.rs
+++ flow_control/matching/guards/src/main.rs
@@ -1,3 +1,17 @@
+fn guard_test() {
+    let pair = (2, -2);
+    println!("Tell me about {:?}", pair);
+
+    // guard는 match의 필터로서 기능함
+    match pair {
+        (x, y) if x == y => println!("These are twins"),
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, _) if x % 2 == 1 => println!("The first one is odd"),
+        _ => println!("No correlation..."),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("\n------ guard_test() ------");
+    guard_test();
 }
